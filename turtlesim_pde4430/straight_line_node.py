a2e1e0f8145cbe16a0abb2a125254865909f31db
Twillur <WilliamKoju@gmail.com>
2025-11-07 17:18:15 +0400

    Update straight_line_node.py

--- turtlesim_pde4430/straight_line_node.py
+++ turtlesim_pde4430/straight_line_node.py
@@ -1,4 +1,3 @@
-#!/usr/bin/env python3
 import rclpy
 from rclpy.node import Node
 from geometry_msgs.msg import Twist
@@ -7,20 +6,12 @@ class StraightLine(Node):
     def __init__(self):
         super().__init__('straight_line_node')
         self.pub = self.create_publisher(Twist, '/turtle1/cmd_vel', 10)
-        self.timer = self.create_timer(0.1, self.move)
-        self.msg = Twist()
-        self.msg.linear.x = 2.0
-        self.msg.angular.z = 0.0
+        self.timer = self.create_timer(0.1, lambda: self.pub.publish(Twist(linear=Twist.Linear(x=2.0), angular=Twist.Angular(z=0.0))))
 
-    def move(self):
-        self.pub.publish(self.msg)
-
-def main(args=None):
-    rclpy.init(args=args)
-    node = StraightLine()
-    rclpy.spin(node)
-    node.destroy_node()
+def main():
+    rclpy.init()
+    rclpy.spin(StraightLine())
     rclpy.shutdown()
 
-if __name__ == '__main__':
+if name == '__main__':
     main()
