10207b8764b758afbc5774ef2835337212b271eb
Twillur <WilliamKoju@gmail.com>
2025-11-07 17:24:35 +0400

    Update circle_node.py

--- turtlesim_pde4430/circle_node.py
+++ turtlesim_pde4430/circle_node.py
@@ -7,13 +7,10 @@ class Circle(Node):
     def __init__(self):
         super().__init__('circle_node')
         self.pub = self.create_publisher(Twist, '/turtle1/cmd_vel', 10)
-        self.timer = self.create_timer(0.1, self.move)
         self.msg = Twist()
         self.msg.linear.x = 2.0
-        self.msg.angular.z = 1.0  # controls radius
-
-    def move(self):
-        self.pub.publish(self.msg)
+        self.msg.angular.z = 1.0
+        self.timer = self.create_timer(0.1, lambda: self.pub.publish(self.msg))
 
 def main(args=None):
     rclpy.init(args=args)
@@ -22,5 +19,5 @@ def main(args=None):
     node.destroy_node()
     rclpy.shutdown()
 
-if __name__ == '__main__':
+if __name__=='__main__':
     main()
